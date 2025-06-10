1b31882db1675686f906ef9c1daa82b5c9b2e05a
snail <snail_dev@163.com>
2025-06-10 22:24:53 +0800

    Elastic数据实体，和es服务交互使用

---
+++ src/Snail.Elastic/DataModels/ElasticBulkModel.cs
@@ -0,0 +1,216 @@
+﻿using System.Runtime.Serialization;
+
+/*  Elasti批量操作相关实体；仅内部使用    */
+namespace Snail.Elastic.DataModels
+{
+    /// <summary>
+    /// ES批量索引操作基类
+    /// </summary>
+    [Serializable]
+    internal abstract class ElasticBulkModel : ISerializable
+    {
+        #region 属性变量
+        /// <summary>
+        /// 批量操作名
+        /// </summary>
+        private readonly string _action;
+
+        /// <summary>
+        /// 例外的JSON序列化值操作
+        /// </summary>
+        protected Action<Dictionary<string, string>>? AddJSONValue { init; get; }
+        /// <summary>
+        /// 要操作的索引主键Id；不可为空
+        /// </summary>
+        public required string Id { init; get; }
+
+        /// <summary>
+        /// 要操作的索引名
+        /// </summary>
+        public string? Index { init; get; }
+
+        /// <summary>
+        /// 要操作的索引路由
+        /// </summary>
+        public required string? Routing { init; get; }
+        #endregion
+
+        #region 构造方法
+        /// <summary>
+        /// 构造方法
+        /// </summary>
+        /// <param name="action"></param>
+        public ElasticBulkModel(string action)
+        {
+            _action = ThrowIfNullOrEmpty(action);
+        }
+        #endregion
+
+        #region ISerializable
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
+        void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            ThrowIfNullOrEmpty(_action);
+            ThrowIfNullOrEmpty(Id);
+            /*  json序列化值
+                update = new
+                {
+                    _id = index.Id,
+                    _index = index.IndexName,
+                    routing = index.OrganizationId
+                }
+             */
+            Dictionary<string, string> dict = new()
+            {
+                ["_id"] = Id
+            };
+            string? tmpStr = Default(Index, null);
+            if (tmpStr != null)
+            {
+                dict["_index"] = tmpStr;
+            }
+            tmpStr = Default(Routing, null);
+            if (tmpStr != null)
+            {
+                dict["routing"] = tmpStr;
+            }
+            AddJSONValue?.Invoke(dict);
+
+            info.AddValue(_action, dict);
+        }
+        #endregion
+    }
+    /// <summary>
+    /// 批量操作：创建索引
+    /// </summary>
+    [Serializable]
+    internal sealed class ElasticBulkCreateModel : ElasticBulkModel, ISerializable
+    {
+        /// <summary>
+        /// 构造方法
+        /// </summary>
+        public ElasticBulkCreateModel() : base("create")
+        {
+        }
+    }
+    /// <summary>
+    /// 批量操作：保存（Index）搜索，创建一个新文档或者替换一个现有的文档
+    /// </summary>
+    [Serializable]
+    internal sealed class ElasticBulkSaveModel : ElasticBulkModel, ISerializable
+    {
+        /// <summary>
+        /// 构造方法
+        /// </summary>
+        public ElasticBulkSaveModel() : base("index")
+        {
+        }
+    }
+    /// <summary>
+    /// 批量操作：更新索引
+    /// </summary>
+    [Serializable]
+    internal sealed class ElasticBulkUpdateModel : ElasticBulkModel, ISerializable
+    {
+        /// <summary>
+        /// 版本冲突时的重试次数，默认不重试
+        /// </summary>
+        public int? ConflictRetry { init; get; }
+
+        /// <summary>
+        /// 构造方法
+        /// </summary>
+        public ElasticBulkUpdateModel() : base("update")
+        {
+            //  额外的JSON序列化值：
+            AddJSONValue = dict =>
+            {
+                if (ConflictRetry > 0)
+                {
+                    dict["retry_on_conflict"] = ConflictRetry.Value.ToString();
+                }
+            };
+        }
+    }
+    /// <summary>
+    /// 批量操作：创建索引
+    /// </summary>
+    [Serializable]
+    internal sealed class ElasticBulkDeleteModel : ElasticBulkModel, ISerializable
+    {
+        /// <summary>
+        /// 构造方法
+        /// </summary>
+        public ElasticBulkDeleteModel() : base("delete")
+        {
+        }
+    }
+
+    /// <summary>
+    /// 批量操作：索引更新脚本实体
+    ///     先仅支持id脚本所有
+    /// </summary>
+    [Serializable]
+    internal sealed class ElasticBuildUpdateScriptModel : ISerializable
+    {
+        /// <summary>
+        /// 要执行的脚本Id
+        /// </summary>
+        public string Id { private init; get; }
+
+        /// <summary>
+        /// 脚本更新时传入参数
+        /// </summary>
+        public object Params { private init; get; }
+
+        /// <summary>
+        /// Json序列化时，是否忽略外层的Script标签
+        ///     为true时，序列化成{id,params}
+        ///     为false时，序列化成{script:{id,params}}
+        /// </summary>
+        public bool JsonIgnoreScriptTag { private init; get; }
+
+        #region 构造方法
+        /// <summary>
+        /// 构造方法
+        /// </summary>
+        /// <param name="id">要执行的脚本Id；不能为null</param>
+        /// <param name="params">脚本更新时传入参数；不能为null</param>
+        /// <param name="jsonIgnoreScriptTag">Json序列化时，是否忽略外层的Script标签</param>
+        public ElasticBuildUpdateScriptModel(string id, object @params, bool jsonIgnoreScriptTag = false)
+        {
+            ThrowIfNullOrEmpty(Id = id);
+            ThrowIfNull(Params = @params);
+            JsonIgnoreScriptTag = jsonIgnoreScriptTag;
+        }
+        #endregion
+
+        #region ISerializable
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
+        void ISerializable.GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            if (JsonIgnoreScriptTag == true)
+            {
+                info.AddValue("id", Id);
+                info.AddValue("params", Params);
+            }
+            else
+            {
+                info.AddValue("script", new
+                {
+                    id = Id,
+                    @params = Params,
+                });
+            }
+        }
+        #endregion
+    }
+}
---
+++ src/Snail.Elastic/DataModels/ElasticSearchResult.cs
@@ -0,0 +1,142 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Snail.Elastic.DataModels
+{
+    /// <summary>
+    /// ElasticSearch数据搜索结果 <br />
+    ///     不用完全把结果属性整理出来，仅限实际需要的
+    /// </summary>
+    /// <typeparam name="SourceModel">索引数据的Source实体类型</typeparam>
+    public sealed class ElasticSearchResult<SourceModel> where SourceModel : class
+    {
+        #region 属性变量
+        /// <summary>
+        /// 搜索花费时长
+        /// </summary>
+        [JsonProperty(PropertyName = "took")]
+        public int Took { set; get; }
+
+        /// <summary>
+        /// 搜索是否超时
+        /// </summary>
+        [JsonProperty(PropertyName = "timed_out")]
+        public bool Timedout { set; get; }
+
+        /// <summary>
+        /// 搜索结果数据
+        /// </summary>
+        [JsonProperty(PropertyName = "hits")]
+        public ElasticSearchHitsModel? Hits { set; get; }
+
+        /// <summary>
+        /// 聚合查询结果
+        ///     这个内部结构不统一，这里不做统一格式要求
+        /// </summary>
+        [JsonProperty(PropertyName = "aggregations")]
+        public JObject? Aggregations { set; get; }
+        #endregion
+
+        #region 内部类
+        /// <summary>
+        /// 数据搜索命中数据结果集
+        /// </summary>
+        public sealed class ElasticSearchHitsModel
+        {
+            /* 超过1w条数据后，这里给的结果.value始终为1w，通过relation区分。
+             *      给值不精确，直接忽略不用
+            /// <summary>
+            /// 搜索结果数据条数
+            ///     符合查询条件的所有数据条数，排除分页影响
+            /// </summary>
+            [JsonProperty(PropertyName = "total")]
+            public ElasticSearchTotalModel Total { set; get; }
+            */
+
+            /// <summary>
+            /// 命中数据的最大分数
+            /// </summary>
+            [JsonProperty(PropertyName = "max_score")]
+            public double? MaxScore { set; get; }
+
+            /// <summary>
+            /// 命中数据集合
+            /// </summary>
+            [JsonProperty(PropertyName = "hits")]
+            public List<ElasticSearchHitModel>? Hits { set; get; }
+        }
+
+        /// <summary>
+        /// 数据搜索结果合计实体
+        /// </summary>
+        public sealed class ElasticSearchTotalModel
+        {
+            /// <summary>
+            /// 
+            /// </summary>
+            [JsonProperty(PropertyName = "value")]
+            public long Value { set; get; }
+
+            /// <summary>
+            /// 
+            /// </summary>
+            [JsonProperty(PropertyName = "relation")]
+            public string? Relation { set; get; }
+        }
+
+        /// <summary>
+        /// 数据搜索命中的单条数据信息实体
+        /// </summary>
+        public sealed class ElasticSearchHitModel
+        {
+            /* 先不放开
+            /// <summary>
+            /// 操作类型
+            /// </summary>
+            [JsonProperty("_type")]
+            public String Type { set; get; }
+             */
+            /// <summary>
+            /// 索引名
+            /// </summary>
+            [JsonProperty("_index")]
+            public required string Index { set; get; }
+
+            /// <summary>
+            /// 数据Id
+            /// </summary>
+            [JsonProperty(PropertyName = "_id")]
+            public required string Id { set; get; }
+
+            /// <summary>
+            /// 数据得分
+            /// </summary>
+            [JsonProperty(PropertyName = "_score")]
+            public double? Score { set; get; }
+
+            /// <summary>
+            /// 数据路由
+            /// </summary>
+            [JsonProperty(PropertyName = "_routing")]
+            public string? Routing { set; get; }
+
+            /// <summary>
+            /// 构建索引时的源数据
+            /// </summary>
+            [JsonProperty(PropertyName = "_source")]
+            public SourceModel? Source { set; get; }
+
+            /// <summary>
+            /// 数据排序值：用于进行search_after查询使用
+            /// </summary>
+            [JsonProperty(PropertyName = "sort")]
+            public List<object>? Sort { set; get; }
+            /// <summary>
+            /// 数据命中的Name查询条件集合
+            /// </summary>
+            [JsonProperty(PropertyName = "matched_queries")]
+            public List<string>? MatchedQueries { set; get; }
+        }
+        #endregion
+    }
+}
