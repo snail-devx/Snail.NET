3f31ae053f2db663a0c5ccda3bba8df1ea60ada9
snail <snail_dev@163.com>
2025-10-04 22:13:54 +0800

    数据库支持elasticsearch

---
+++ src/Snail.Elastic/Components/ElasticDeletable.cs
@@ -0,0 +1,51 @@
+﻿using Snail.Database.Components;
+using Snail.Elastic.DataModels;
+
+namespace Snail.Elastic.Components
+{
+    /// <summary>
+    /// <see cref="IDbDeletable{DbModel}"/>接口的Elastic实现
+    /// </summary>
+    /// <typeparam name="DbModel">数据库实体；需被<see cref="DbTableAttribute"/>特性标记</typeparam>
+    public class ElasticDeletable<DbModel> : DbDeletable<DbModel>, IDbDeletable<DbModel> where DbModel : class
+    {
+        #region 属性变量
+        /// <summary>
+        /// Elasti运行器
+        /// </summary>
+        protected readonly ElasticModelRunner<DbModel> Runner;
+        /// <summary>
+        /// 过滤条件构建器
+        /// </summary>
+        protected readonly ElasticFilterBuilder<DbModel> FilterBuilder;
+        #endregion
+
+        #region 构造方法
+        /// <summary>
+        /// 构造方法
+        /// </summary>
+        /// <param name="runner">运行器</param>
+        /// <param name="builder">过滤条件构建器；为null则使用默认的<see cref="ElasticFilterBuilder{DbModel}"/></param>
+        /// <param name="routing">路由信息</param>
+        public ElasticDeletable(ElasticModelRunner<DbModel> runner, ElasticFilterBuilder<DbModel>? builder, string? routing)
+            : base(routing)
+        {
+            Runner = ThrowIfNull(runner);
+            FilterBuilder = builder ?? ElasticFilterBuilder<DbModel>.Default;
+        }
+        #endregion
+
+        #region IDbDeletable：部分交给【DbDeletable】做默认实现
+        /// <summary>
+        /// 执行删除操作
+        /// </summary>
+        /// <remarks>禁止无条件删除</remarks>
+        /// <returns>删除数据条数</returns>
+        public async override Task<long> Delete()
+        {
+            ElasticQueryModel query = FilterBuilder.BuildFilter(Filters);
+            return await Runner.DeleteByQuery(Routing, query);
+        }
+        #endregion
+    }
+}
---
+++ src/Snail.Elastic/Utils/ElasticHelper.cs
@@ -0,0 +1,99 @@
+﻿using System.Diagnostics;
+using System.Text;
+using Snail.Abstractions.Database.DataModels;
+using Snail.Abstractions.Logging;
+using Snail.Abstractions.Logging.Extensions;
+using Snail.Abstractions.Web.DataModels;
+using Snail.Utilities.Collections.Extensions;
+using Snail.Web.Components;
+
+namespace Snail.Elastic.Utils
+{
+    /// <summary>
+    /// ElasticSearch助手类
+    /// </summary>
+    public static class ElasticHelper
+    {
+        #region 属性变量
+        /// <summary>
+        /// URL参数：仅需要Source字段值
+        /// </summary>
+        public const string PARAM_OnlySource = "filter_path=hits.hits._source";
+        /// <summary>
+        /// 最大数据量
+        /// </summary>
+        public const int MAX_Size = 10000;
+        #endregion
+
+        #region 公共方法
+        /// <summary>
+        /// 构建Elastic操作
+        /// </summary>
+        /// <param name="tableName">表名：es索引名；若不指定则全库操作，有安全隐患；会强制小写</param>
+        /// <param name="action">要执行的操作名；不可为空；</param>
+        /// <param name="routing">路由名；为null则不在API上追加 routing={routing}参数</param>
+        /// <param name="urlParams">url上附带的参数数组，格式 key=value</param>
+        /// <returns>构建好的操作api地址</returns>
+        public static string BuildAction(in string tableName, in string action, in string? routing, IList<string>? urlParams = null)
+        {
+            //  action强制不能为空
+            ThrowIfNullOrEmpty(action);
+            if (routing?.Length > 0)
+            {
+                urlParams ??= new List<string>();
+                urlParams.Insert(0, $"routing={routing}");
+            }
+            return tableName?.Length > 0
+                ? $"/{tableName.ToLower()}/{action}?{urlParams?.AsString('&')}"
+                : $"/{action}?{urlParams?.AsString('&')}";
+        }
+
+        //  后期在这里做一些其他通用逻辑，单纯不带日志记录的；如通用的Search、Insert、Save、Bulk、、、
+
+        /// <summary>
+        /// 执行Post发送字符串数据；内部会记录post和response数据
+        /// </summary>
+        /// <param name="title">操作标题，用作日志标题</param>
+        /// <param name="dbServer">数据库服务器</param>
+        /// <param name="api">api地址</param>
+        /// <param name="post">post数据</param>
+        /// <param name="logger">日志记录器</param>
+        /// <param name="logPost">是否记录post数据；若post数据量特别大，则考虑false；否则影响性能</param>
+        /// <param name="logResult">是否记录结果日志；若结果数据量特别大，则考虑false；否则影响性能</param>
+        /// <returns>post请求结果</returns>
+        public static async Task<string?> PostString(string title, DbServerDescriptor dbServer, string api, string post, ILogger logger, bool logPost = true, bool logResult = true)
+        {
+            string? ret = null;
+            Stopwatch sw = Stopwatch.StartNew();
+            Exception? tmpEx = null;
+            try
+            {
+                Uri baseAddress = new(dbServer.Connection);
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, api)
+                {
+                    Content = new StringContent(post, Encoding.UTF8, "application/json")
+                };
+                HttpResponseMessage response = await HttpProvider.Send(baseAddress, request);
+                HttpResult hr = new HttpResult(response);
+                ret = await hr.AsStringAsync;
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                tmpEx = ex;
+                throw;
+            }
+            finally
+            {
+                sw.Stop();
+                post = logPost ? post : "调用方约束不记录post数据";
+                ret = logResult ? ret : "调用方约束不记录response结果";
+                string logContent = $"api:{api}; performance:{sw.ElapsedMilliseconds}; post:{post}; ret:{ret}";
+                var _ = tmpEx == null
+                    ? logger.Trace(title, logContent)
+                    : logger.Error(title, logContent, tmpEx);
+            }
+        }
+        #endregion
+    }
+}
