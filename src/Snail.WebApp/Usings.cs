6ec3e1033577f3103efbb47734618ce1e579243e
snail <snail_dev@163.com>
2025-11-21 22:14:17 +0800

    添加常用的全局using声明

--- src/Snail.WebApp/Usings.cs
+++ src/Snail.WebApp/Usings.cs
@@ -3,12 +3,16 @@ global using System.Reflection;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.AspNetCore.Mvc.Abstractions;
 global using Microsoft.AspNetCore.Mvc.Controllers;
+global using Microsoft.AspNetCore.Mvc.Filters;
 //  基础通用
 global using Snail.Abstractions;
 global using Snail.Abstractions.Dependency;
+global using Snail.Abstractions.Dependency.Attributes;
 global using Snail.Abstractions.Dependency.Enumerations;
 global using Snail.Abstractions.Dependency.Extensions;
 global using Snail.Common.Components;
 global using Snail.Utilities.Common;
+global using static Snail.Abstractions.ConstString;
 //  引入静态助手类方法
 global using static Snail.Utilities.Common.Utils.ObjectHelper;
+global using static Snail.Utilities.Common.Utils.StringHelper;
