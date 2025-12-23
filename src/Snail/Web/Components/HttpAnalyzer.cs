a275ab7d047ac22b7669441858133e061a465526
snail <snail_dev@163.com>
2025-12-23 20:57:39 +0800

    优化异步逻辑

--- src/Snail/Web/Components/HttpAnalyzer.cs
+++ src/Snail/Web/Components/HttpAnalyzer.cs
@@ -16,15 +16,10 @@ public class HttpAnalyzer : IHttpAnalyzer
     /// <param name="parameters">外部传入的已有参数字典；key为参数名、value为具体参数值</param>
     /// <remarks>处理时url参数不区分大小写</remarks>
     /// <returns>处理后的url地址</returns>
-    public virtual async Task<string> AnalysisUrl(string url, IDictionary<string, object?>? parameters)
+    public virtual Task<string> AnalysisUrl(string url, IDictionary<string, object?>? parameters)
     {
-        //  做一个加的异步等待，url为null的情况不可能存在，除非传错了
-        if (url == null)
-        {
-            await Task.Yield();
-        }
-
-        return ParameterAnalyzer.DEFAULT.Analysis(url, parameters)!;
+        url = ParameterAnalyzer.DEFAULT.Analysis(url, parameters) ?? string.Empty;
+        return Task.FromResult(url);
     }
     #endregion
 }
